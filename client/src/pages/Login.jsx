e51462402bfdda13d2dddbfee8fdbb5a6a30cb04
ritikjee <ritikj.ug21.ee@nitp.ac.in>
2024-06-27 08:32:50 +0530

    added login and register page

---
+++ client/src/pages/Login.jsx
@@ -0,0 +1,73 @@
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import { Link } from "react-router-dom";
+import { useState } from "react";
+import { toast } from "sonner";
+
+function Login() {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  return (
+    <div className="flex  items-center justify-center h-screen w-screen">
+      <Card className="md:w-96  ">
+        <CardHeader>
+          <CardTitle>Login </CardTitle>
+          <CardDescription>
+            New to medium?{" "}
+            <Link to={"/register"} className="underline">
+              Register
+            </Link>
+          </CardDescription>
+        </CardHeader>
+        <CardContent>
+          <form>
+            <div className="grid w-full items-center gap-4">
+              <div className="flex flex-col items-start gap-1 space-y-1.5">
+                <Label htmlFor="username">Username</Label>
+                <Input
+                  id="username"
+                  placeholder="Username"
+                  name="username"
+                  value={username}
+                  onChange={(e) => setUsername(e.target.value)}
+                />
+              </div>
+
+              <div className="flex flex-col items-start gap-1 space-y-1.5">
+                <Label htmlFor="password">Password</Label>
+                <Input
+                  id="password"
+                  placeholder="Enter your password"
+                  name="password"
+                  value={password}
+                  onChange={(e) => setPassword(e.target.value)}
+                />
+              </div>
+            </div>
+          </form>
+        </CardContent>
+        <CardFooter className="flex justify-between">
+          <Link to={"/"}>
+            <Button variant="outline">Cancel</Button>
+          </Link>
+          <Button onClick={() => toast.success("This is just a text toast")}>
+            Submit
+          </Button>
+        </CardFooter>
+      </Card>
+    </div>
+  );
+}
+
+export default Login;
