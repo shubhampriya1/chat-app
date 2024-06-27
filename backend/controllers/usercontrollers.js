4c66f9fbced43a9b92357e47e7c9485c24179b5d
shubhampriya1 <shubhamp.ug21.ee@nitp.ac.in>
2024-06-27 18:49:38 +0530

    second change

--- backend/controllers/usercontrollers.js
+++ backend/controllers/usercontrollers.js
@@ -3,7 +3,7 @@ import { User } from "../model/user.model.js";
 export async function register(req, res) {
   try {
     const { name, email, password, pic } = req.body;
-    if (!name || !email || !password || !pic) {
+    if (!name || !email || !password ) {
       return res.status(400).send("please your deatails");
     }
     const useremail = await User.find({
@@ -26,6 +26,37 @@ export async function register(req, res) {
       email: newuser.email,
       name: newuser.name,
       pic: newuser.pic,
+    });
+  } catch (error) {
+    console.log(error);
+    return res.status(500).send("Internal server error");
+  }
+}
+
+export async function login(req, res) {
+  try {
+
+  } catch (error) {
+    console.log(error);
+    return res.status(500).send("Internal server error");
+  }
+}
+export async function login(req, res) {
+  try {
+    const { email, password } = req.body;
+    if (!email || !password) {
+      return res.status(400).send("please fill ur details");
+    }
+    const user = await User.findOne({
+      $or: [{ email: identifier }],
+    });
+    if (!user) {
+      return res.status(404).send("No user with provided credentials found");
+    }
+    res.status(200).json({
+      userId: user._id,
+      username: user.username,
+      useremail: user.email,
     });
   } catch (error) {
     console.log(error);
--- backend/routers/userRoutes.js
+++ backend/routers/userRoutes.js
@@ -1,7 +1,8 @@
-import express from "express";
+
 import { Router } from "express";
 import { register } from "../controllers/usercontrollers.js";
+// import { login } from "../controllers/logincontroller.js";
 const authrouter = Router();
 authrouter.route("/").post(register);
-// authrouter.route("/login").post(login);
+authrouter.route("/login").post(login);
 export default authrouter;
