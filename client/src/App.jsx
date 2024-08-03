4e5ad25058a0bda026d5a06abbce25cd42072494
Shubham priya <141516634+shubhampriya1@users.noreply.github.com>
2024-08-03 23:21:19 +0530

    Completed socket.io

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,23 +2,38 @@ import "./App.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 import Chatpage from "./pages/Chatpage";
+import PublicRoute from "./components/PublicRoute";
+import PrivateRouter from "./components/PrivateRoute";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
 import Search from "./pages/Search";
 import Message from "./pages/Message";
 import Page404 from "./pages/NotFound";
-import PrivateRouter from "./components/PrivateRoute";
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Login />} />
-        <Route path="/register" element={<Register />} />
+        <Route
+          path="/"
+          element={
+            <PublicRoute>
+              <Login />
+            </PublicRoute>
+          }
+        />
+        <Route
+          path="/register"
+          element={
+            <PublicRoute>
+              <Register />
+            </PublicRoute>
+          }
+        />
         <Route path="/" element={<PrivateRouter />}>
           <Route path="/chats" element={<Chatpage />} />
-          <Route path="/search" element={<Search />} />
           <Route path="/chats/:chatId" element={<Message />} />
+          <Route path="/search" element={<Search />} />
         </Route>
         <Route path="*" element={<Page404 />} />
       </Routes>
