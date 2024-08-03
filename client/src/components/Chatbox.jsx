288ab452727937f8bd6c2899c977134e786ab063
Shubham priya <141516634+shubhampriya1@users.noreply.github.com>
2024-08-03 23:03:20 +0530

    Update Chatbox for live connection

--- client/src/components/Chatbox.jsx
+++ client/src/components/Chatbox.jsx
@@ -5,12 +5,17 @@ import { Search, Settings, EllipsisVertical, Send } from "lucide-react";
 import axios from "axios";
 import { Input } from "./ui/input";
 import PropType from "prop-types";
+import { useSocket } from "@/hooks/use-socket";
+import { useAuth } from "@/hooks/use-auth";
 
 const Chatbox = (props) => {
   const [newMessage, setNewMessage] = useState("");
   const [userData, setUserData] = useState({});
   const [resultmessage, setResultmessage] = useState([]);
   const [chatId, setChatId] = useState("");
+
+  const { socket } = useSocket();
+  const { user } = useAuth();
 
   const backendUrl = import.meta.env.VITE_PUBLIC_BACKEND_URL;
   async function fetchMessage() {
@@ -48,8 +53,11 @@ const Chatbox = (props) => {
 
   useEffect(() => {
     fetchMessage();
+    socket.emit("setup", chatId);
   }, [props.chatId]);
-  async function sendMessage() {
+
+  async function sendMessage(e) {
+    e.preventDefault();
     try {
       const token = Cookies.get("authtoken");
       if (!token) {
@@ -62,12 +70,29 @@ const Chatbox = (props) => {
       };
       const body = { content: newMessage, chatId: chatId };
       await axios.post(`${backendUrl}/api/message`, body, config);
+      socket.emit("message", {
+        chatId,
+        message: {
+          _id: Math.random(),
+          content: newMessage,
+          sender: {
+            ...user,
+          },
+        },
+      });
       setNewMessage("");
-      fetchMessage();
     } catch (error) {
       console.log("error", error);
     }
   }
+
+  useEffect(() => {
+    socket.on("message", (message) => {
+      console.log(message);
+      setResultmessage((prev) => [...prev, message]);
+    });
+  }, []);
+
   return (
     <div className="flex flex-col h-screen ">
       <div className=" w-full py-3 px-3 border-2 flex justify-between rounded-md  bg-slate-600">
@@ -89,41 +114,36 @@ const Chatbox = (props) => {
           <EllipsisVertical />
         </div>
       </div>
-      <div className="mr-8  flex-1 gap-5 flex flex-col max-h-full overflow-y-scroll mt-5 px-2">
+      <div className="  flex-1 gap-5 flex flex-col max-h-full overflow-y-scroll mt-5 px-5">
         {resultmessage.map((item, index) => (
           <div
             key={index}
             className={`flex gap-3 items-center ${
-              resultmessage?.sender?.id === props.chatId
+              item?.sender?._id === props.chatId
                 ? "justify-start"
                 : "justify-end"
             }`}
           >
-            <img
-              className="h-10 w-10 rounded-full"
-              src="https://github.com/shadcn.png"
-            />
-            <div>
-              <h1 className="border-2 rounded-md px-5 py-2">{item.content}</h1>
-            </div>
+            <h1 className="border-2 rounded-md px-5 py-2">{item.content}</h1>
           </div>
         ))}
       </div>
 
-      <div className="flex">
+      <form className="flex">
         <Input
           className="mb-1 mr-1 "
           placeholder="Enter your message"
           value={newMessage}
           onChange={(e) => setNewMessage(e.target.value)}
         />
-        <div
+        <button
           className="bg-blue-500 rounded-md w-10 mb-1 "
+          type="submit"
           onClick={sendMessage}
         >
           <Send className="mt-3 ml-1" />
-        </div>
-      </div>
+        </button>
+      </form>
     </div>
   );
 };
